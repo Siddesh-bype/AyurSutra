7b0ed4905cb93aa65e2baa4389e1d1b2dd66b35d
siddeshshirote30052006 <siddeshshirote30052006@gmail.com>
2025-09-24 14:26:10 +0000

    all

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,10 +12,10 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  HomeScreenState createState() => HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:myapp/constants/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
